449da69d3f9a9cfc708b04f821b4ee9a5f0b1a2a
David Jones <david.jones@madwiremedia.com>
2017-05-01 21:22:32 -0600

    EJS

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var express = require('express')
 var app = express()
+var bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
@@ -8,6 +12,20 @@ app.set('view engine', 'ejs');
 // app.get('/', function (req, res) {
 //   res.send('Hello World!');
 // })
+
+app.get("/signup", function(req,res){
+  res.render("email-signup")
+})
+app.get("/the_user_submitted_the_form",function(req,res){
+  console.log(req.query);
+  res.send("Thank you for submitting, "+req.query.the_email_that_the_user_submitted);
+})
+
+app.post("/the_user_submitted_the_form_post",function(req,res){
+  console.log(req.body);
+  res.send("Thank you for submitting, "+ req.body.the_email_that_the_user_submitted);
+})
+
 
 app.get("/answer", function(req,res){
   var answer = -5042 + 5084;
